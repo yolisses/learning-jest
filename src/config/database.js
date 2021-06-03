59c52438d0ee0e9a4c3cc9a793e3a333bf19b4e1
Ulisses <m.stunik@gmail.com>
2021-06-03 11:03:24 -0300

    create database connection and user model

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+	host: '127.0.0.1',
+	username: 'postgres',
+	password: 'postgres',
+	database: 'learning-jest',
+	dialect: 'postgres',
+	operatorAliases: false,
+	logging: false,
+	define: {
+		timestamps: true,
+		underscored: true,
+		underscoredAll: true,
+	},
+};
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+const Router = require('express');
+const { User } = require('./app/models');
+
+const router = Router();
+
+module.exports = router;
